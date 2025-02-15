b7199c8c9fb56c1b643efd51bc5bffe2ea2c048d
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-02-15 22:06:40 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allow access from any network interface
+    port: 5173, // Your desired port
+    allowedHosts: ['pearlsoftech.com'], // Add your domain here
+  },
 })
