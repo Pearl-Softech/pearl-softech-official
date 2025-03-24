5db9b00dc8c560641581990808775c8a183392cd
devRajeshThapa <devrajeshthapa@gmail.com>
2025-03-24 13:20:14 +0545

    Enhance

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -3,18 +3,10 @@ import '../styles/Admin.css';
 import { PulseLoader } from 'react-spinners';
 
 const Admin = () => {
-  const [dots, setDots] = useState('');
 
   useEffect(() => {
     // Start redirection immediately
     window.location.href = 'http://54.161.35.160:5174/';
-
-    // Optional: Dots animation (won't be visible because of instant redirect)
-    const dotInterval = setInterval(() => {
-      setDots(prev => (prev.length < 3 ? prev + '.' : ''));
-    }, 500);
-
-    return () => clearInterval(dotInterval);
   }, []);
 
   return (
