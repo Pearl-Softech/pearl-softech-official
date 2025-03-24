f8a9b30db43ce4c93f99dbb8bf0267f79c69acf9
devRajeshThapa <devrajeshthapa@gmail.com>
2025-03-24 10:45:42 +0545

    Enhancement

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import '../styles/Blog.css'
+import PulseLoader from "react-spinners/PulseLoader";
+import '../styles/Blog.css';
 
 const Blog = () => {
     const API_KEY = import.meta.env.VITE_API_KEY;
-  const SERVER_IP = import.meta.env.VITE_SERVER_IP;
-    const { id } = useParams(); // Get the `id` from the URL
-    const [blogPost, setBlogPost] = useState(null); // State to store the blog post data
-    const [loading, setLoading] = useState(true); // Loading state for API call
-    const [error, setError] = useState(null); // Error state in case the API fails
+    const SERVER_IP = import.meta.env.VITE_SERVER_IP;
+    const { id } = useParams();
+    const [blogPost, setBlogPost] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         const fetchBlogPost = async () => {
@@ -22,35 +23,38 @@ const Blog = () => {
                 if (!response.ok) {
                     throw new Error('Failed to fetch blog post');
                 }
-                const data = await response.json(); // Assuming the API returns JSON data
-                setBlogPost(data.blog); // Set the blog post data into the state
+                const data = await response.json();
+                setBlogPost(data.blog);
             } catch (error) {
-                setError(error.message); // Set error message if API fails
+                setError(error.message);
             } finally {
-                setLoading(false); // Set loading to false after the fetch completes
+                setLoading(false);
             }
         };
 
-        const incrementViews = async ()=>{
-            try{
-                const response = await fetch(`${SERVER_IP}/blog/increment-views/${id}`, {
+        const incrementViews = async () => {
+            try {
+                await fetch(`${SERVER_IP}/blog/increment-views/${id}`, {
                     method: "PUT",
                     headers: {
                         'x-api-key': API_KEY
                     }
                 });
-                console.log(response)
-            }catch(err){
+            } catch (err) {
                 console.log(err);
             }
         }
 
         fetchBlogPost();
         incrementViews();
-    }, [id]); // Re-run when the `id` changes
+    }, [id]);
 
     if (loading) {
-        return <div>Loading...</div>;
+        return (
+            <div className="loader-container" style={{ display: 'flex', width: "100%", height: "100dvh", justifyContent: 'center', alignItems: "center" }}>
+                <PulseLoader color="#16364a" size={15} />
+            </div>
+        );
     }
 
     if (error) {
@@ -66,18 +70,18 @@ const Blog = () => {
             <main>
                 <h1 className="blog-card-title">{blogPost.title}</h1>
                 <div className='date-views-wrapper'>
-                <p className="blog-date">
-                    <i className="fa-solid fa-calendar-days"></i>
-                    {new Date(blogPost.createdAt).toLocaleDateString('en-US', {
-                        year: 'numeric',
-                        month: 'long',
-                        day: 'numeric'
-                    })}
-                </p>
-                <p className='blog-views'>
-                    <i className='fa-solid fa-eye'></i>
-                    {blogPost.views + " Views"}
-                </p>
+                    <p className="blog-date">
+                        <i className="fa-solid fa-calendar-days"></i>
+                        {new Date(blogPost.createdAt).toLocaleDateString('en-US', {
+                            year: 'numeric',
+                            month: 'long',
+                            day: 'numeric'
+                        })}
+                    </p>
+                    <p className='blog-views'>
+                        <i className='fa-solid fa-eye'></i>
+                        {blogPost.views + " Views"}
+                    </p>
                 </div>
                 <div className="blog-thumbnail">
                     <img src={blogPost.thumbnail} alt={blogPost.title} />
@@ -89,7 +93,7 @@ const Blog = () => {
                 <div className="right">
                     <a href={"https://www.facebook.com/sharer/sharer.php?u=" + "pearlsoftech.com" + "/blog/" + blogPost._id} target='_blank'><i className='fa-brands fa-facebook'></i></a>
                     <a href={"https://wa.me/?text=" + "pearlsoftech.com" + "/blog/" + blogPost._id} target='_blank'><i className='fa-brands fa-whatsapp'></i></a>
-                    <a href={"https://www.linkedin.com/shareArticle?mini=true&url="+ "pearlsoftech.com" + "/blog/" + blogPost._id +"&title=Optional%20Title&summary=Optional%20Summary&source=Optional%20Source"} target='_blank'><i className='fa-brands fa-linkedin'></i></a>
+                    <a href={"https://www.linkedin.com/shareArticle?mini=true&url=" + "pearlsoftech.com" + "/blog/" + blogPost._id + "&title=Optional%20Title&summary=Optional%20Summary&source=Optional%20Source"} target='_blank'><i className='fa-brands fa-linkedin'></i></a>
                     <a href={"mailto:?subject=Check%20this%20out&body=Check%20out%20this%20Blog%3A%20" + "pearlsoftech.com" + "/blog/" + blogPost._id} target='_blank'><i className='fa-solid fa-envelope'></i></a>
                 </div>
             </div>
--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -1,20 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Blogs.css';
+import { PulseLoader } from 'react-spinners';
 
 function Blogs() {
   const API_KEY = import.meta.env.VITE_API_KEY;
   const SERVER_IP = import.meta.env.VITE_SERVER_IP;
   const [blogs, setBlogs] = useState([]);
-  const [loadingDots, setLoadingDots] = useState('');
-
-  // Dots animation effect for loading
-  useEffect(() => {
-    const dotInterval = setInterval(() => {
-      setLoadingDots(prev => (prev.length < 3 ? prev + '.' : ''));
-    }, 500);
-    return () => clearInterval(dotInterval);
-  }, []);
 
   useEffect(() => {
     const fetchBlogs = async () => {
@@ -42,12 +34,15 @@ function Blogs() {
     <div className="blogs-container">
       <h1 className="blog-title">OUR BLOGS</h1>
       {
-        blogs.length !=0?
-        null
-        :
-        <div style={{color: "var(--primary-color)", display:'flex', width:"100%", height: "65dvh", justifyContent:'center', alignItems: "center"}}>
-          <h2>LOADING{loadingDots}</h2>
-        </div>
+        blogs.length != 0 ?
+          null
+          :
+          <div style={{ display: 'flex', width: "100%", height: "70dvh", justifyContent: 'center', alignItems: "center"}}>
+
+            <PulseLoader
+            color='#16364a'
+            />
+          </div>
       }
       <div className="blog-flex-box">
         {blogs.map((blog) => (
@@ -55,19 +50,19 @@ function Blogs() {
             <img src={blog.thumbnail} alt={blog.title} className="blog-thumbnail" />
             <h2 className="blog-card-title">{blog.title}</h2>
             <div className='date-views-wrapper'>
-                <p className="blog-date">
-                    <i className="fa-solid fa-calendar-days"></i>
-                    {new Date(blog.createdAt).toLocaleDateString('en-US', {
-                        year: 'numeric',
-                        month: 'long',
-                        day: 'numeric'
-                    })}
-                </p>
-                <p className='blog-views'>
-                    <i className='fa-solid fa-eye'></i>
-                    {blog.views + " Views"}
-                </p>
-                </div>
+              <p className="blog-date">
+                <i className="fa-solid fa-calendar-days"></i>
+                {new Date(blog.createdAt).toLocaleDateString('en-US', {
+                  year: 'numeric',
+                  month: 'long',
+                  day: 'numeric'
+                })}
+              </p>
+              <p className='blog-views'>
+                <i className='fa-solid fa-eye'></i>
+                {blog.views + " Views"}
+              </p>
+            </div>
 
             <p className="blog-body">
               {blog.body.length > 150 ? blog.body.substring(0, 150) + '...' : blog.body}
